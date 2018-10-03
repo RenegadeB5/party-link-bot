6152e9aeb6931a004d6275fe2efde2914577f632
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-03 19:39:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ client.on ('message', message => {
       else {
               message.author.send('You are not authorized to post links.');
           }          
-}});    
+});    
 
 client.on('messageReactionAdd', (reaction, user) => {
     if(reaction.emoji.name === '🔗') {
