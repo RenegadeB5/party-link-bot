cea2edc69a7f78d03dc2cd341c6e38157672ec09
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-02 20:23:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 });     
 
 client.on ('message', message => { 
-  if (message.attachments.size >= 1) {
+  if ((message.channel === message.guild.channels.find("name", "art")&&(message.attachments.size >= 1)) {
       message.react('🔺');
       console.log(message.attachments.size);
   }
