82e9a3fec1cc7ff57f221afc5603a02402c71b1c
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-03 20:00:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,8 +44,7 @@ client.on ('message', message => {
         }
         else {      
             message.author.send('You are not authorized to post links.').catch(error)
-            }
-        }          
+            }          
 });    
 
 client.on('messageReactionAdd', (reaction, user) => {
